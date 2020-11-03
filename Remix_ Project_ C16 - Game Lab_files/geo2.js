6234e359511072457ba71853ff1d1a39f2d57cf4
madhurima23 <73743998+madhurima23@users.noreply.github.com>
2020-11-03 16:28:54 +0530

    Add files via upload

---
+++ Remix_ Project_ C16 - Game Lab_files/geo2.js
@@ -0,0 +1,16 @@
+(function(){
+  window['optimizely'] = window['optimizely'] || [];
+  window['optimizely'].push(['activateGeoDelayedExperiments', {
+    'location':{
+      'city': "SANFRANCISCO",
+      'continent': "NA",
+      'country': "US",
+      'region': "CA"
+    },
+    'ip':"8.20.101.29"
+  }]);
+})
+//
+()
+
+;
